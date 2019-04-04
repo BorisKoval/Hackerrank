741248c9adbb6418e68c82d7338724f023c22803
Boris <boris_bfh@mail.ru>
2019-04-04 22:51:26 +0300

    fix

--- Tasks.py
+++ Tasks.py
@@ -1,35 +1,45 @@
 import sys
 
-""" 
-class TasksSolves():
-    
+class TasksSolves():    
     
     @staticmethod
     def dict_and_maps(path):
+        _input = ''
+        input_dict = {}
 
         with open(path) as inp:
             _input = inp.read()
 
-        input_strings = _input.split('\n')
-        
-        
-        
-        return input_strings
+        n = int(_input.split('\n')[0])
+        input_list = _input.split('\n')
 
+        for i in range(1, n+1):
+            key = input_list[i].split(' ')[0]
+            value = input_list[i].split(' ')[1]
+            input_dict[key] = value
+            
+        for i in range(n+1, len(input_list)):
+            input_dict[key] = value
+            line = input_list[i]
+            if (line in input_dict.keys()):
+                print("%s=%s" % (line, input_dict[line]))
+            else:
+                print("Not found")
 
 path = 'Inputs/day8_input.txt'
 print(TasksSolves.dict_and_maps(path))
 
- """
 
-input_str = ''
+""" 
 lines = input()
+input_str = ''
 
 for i in range(0, int(lines)):
      input_str+= input()
 
-#input_strings = input_str.split('\n')
+input_strings = input_str.split('\n')
 
-#some_dict = input_strings[1:int(input_strings[0])].split(' ')
+some_dict = input_strings[1:int(input_strings[0])].split(' ')
 
 print(input_str)
+ """
